38d70c0037467f314941d4d7cfe969fb61ce0dfb
Egor Astakhov <noise718@gmail.com>
2021-10-11 15:49:38 +0300

    Finished the project

--- Quiz 2021/Models/Question.swift
+++ Quiz 2021/Models/Question.swift
@@ -10,22 +10,22 @@ import Foundation
 struct Question {
     let text: String
     let type: ResponseType
-    let answer: [Answer]
+    let answers: [Answer]
     
     static let all: [Question] = [
-        Question(text: "Какую еду вы предпочитаете", type: .single, answer: [
+        Question(text: "Какую еду вы предпочитаете ?", type: .single, answers: [
             Answer(text: "Стейк", type: .dog),
             Answer(text: "Рыбу", type: .cat),
             Answer(text: "Морковку", type: .rabbit),
             Answer(text: "Кукурузу", type: .turtle),
         ]),
-        Question(text: "Что вы любите делать", type: .multiply, answer: [
+        Question(text: "Что вы любите делать ?", type: .multiple, answers: [
             Answer(text: "Плавать", type: .turtle),
             Answer(text: "Спать", type: .cat),
             Answer(text: "Обниматься", type: .rabbit),
             Answer(text: "Есть", type: .dog),
         ]),
-        Question(text: "Любите ли вы поездки на машине", type: .range, answer: [
+        Question(text: "Любите ли вы поездки на машине ?", type: .range, answers: [
             Answer(text: "Ненавижу", type: .cat),
             Answer(text: "Нервничаю", type: .rabbit),
             Answer(text: "Не замечаю", type: .turtle),
--- Quiz 2021/Models/ResponseType.swift
+++ Quiz 2021/Models/ResponseType.swift
@@ -8,5 +8,5 @@
 import Foundation
 
 enum ResponseType {
-    case single, multiply, range
+    case single, multiple, range
 }
